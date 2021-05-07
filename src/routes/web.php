05dacd0ff70a0beeb77e231fc82f1196101b7ac1
Oluwagbenga Matthew Afolabi <gmattworld@gmail.com>
2021-05-07 14:20:53 +0100

    Initial

---
+++ src/routes/web.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+// Route::get('test', [TestController::class, 'index']);
+
+// Route::post('testpost', function (Request $request) {
+//     $request->all();
+// });
