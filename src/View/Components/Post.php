74fe36adf41b12816832ad258ce3d34970d10a94
Oluwagbenga Matthew Afolabi <gmattworld@gmail.com>
2021-05-13 12:47:06 +0100

    Add prism to code display

---
+++ src/View/Components/Post.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Gmattworld\Blogger\View\Components;
+
+use Illuminate\View\Component;
+
+class Post extends Component
+{
+  public $model;
+  /**
+   * Create a new component instance.
+   *
+   * @return void
+   */
+  public function __construct($model)
+  {
+    $this->model = $model;
+  }
+
+  /**
+   * Get the view / contents that represent the component.
+   *
+   * @return \Illuminate\Contracts\View\View|string
+   */
+  public function render()
+  {
+      return view('blogger::components.post');
+  }
+}
--- src/routes/web.php
+++ src/routes/web.php
@@ -5,19 +5,15 @@ use Gmattworld\Blogger\Http\Controllers\BloggerAdminController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::group(['middleware' => ['web']], function () {
+Route::group(['middleware' => ['web', 'auth']], function () {
   Route::prefix('blogger')->group(function () {  
     Route::get('/', [BloggerController::class, 'posts']);
     Route::get('/{id}', [BloggerController::class, 'post']);
   });
 
 
-  Route::prefix('admin/blogger')->group(function () {
-    Route::resource('/', BloggerAdminController::class);
-    Route::get('/{id}/status', [BloggerAdminController::class, 'status']);
+  Route::prefix('admin')->group(function () {
+    Route::resource('/blogger', BloggerAdminController::class);
+    Route::get('/blogger/{id}/status', [BloggerAdminController::class, 'status']);
   });
-
-// Route::post('testpost', function (Request $request) {
-//     $request->all();
-// });
 });
